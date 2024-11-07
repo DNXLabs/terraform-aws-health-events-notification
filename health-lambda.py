e337ad376489fe7ffeb51ed9c07e86d278c6f5dc
kelvin <kelvin.lopes@dnx.solutions>
2024-11-07 13:34:27 +1100

    standardization

--- health-lambda.py
+++ health-lambda.py
@@ -6,10 +6,16 @@ def lambda_handler(event, context):
     sns_client = boto3.client('sns')
     topic_arn = os.environ.get('SNS_TOPIC_ARN')
     event_rule_name = os.environ.get('EVENT_RULE_NAME')
-
-    rule_base = event_rule_name.replace("dnx-aws-health-event-", "").replace("-ec2-instance-state-change", "")
     
-    event_subject = f' ALARM: "dnx-aws-health-event-ec2-instance-state-change-{rule_base}" in {event["region"]}'
+    # Get customer name from environment variable
+    customer_name = event_rule_name.split('-')[-1]
+    
+    # Format the event rule name with customer name
+    formatted_rule_name = f"dnx-aws-health-event-{event['detail']['eventTypeCode']}-{customer_name}"
+    
+    # Format the subject line with a single set of brackets
+    event_subject = f' ALARM: "dnx-aws-health-event-{event["detail"]["eventTypeCode"]}-{customer_name}" in {event["region"]}'
+    
     event_message = "".join(
         [
             f'{event["detail"]["service"]}\n',
@@ -18,7 +24,7 @@ def lambda_handler(event, context):
             event["detail"]["eventDescription"][0]["latestDescription"],
         ]
     )
-
+    
     response = sns_client.publish(
         TopicArn=topic_arn,
         Message=event_message,
@@ -30,5 +36,4 @@ def lambda_handler(event, context):
             }
         }
     )
-
     print(response)
