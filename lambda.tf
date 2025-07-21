0e4fed77580af0cba3b583c34db5cb60b9a5a81a
Lucas de Macedo <lucas.macedo@dnx.solutions>
2025-07-21 14:15:20 -0300

    add ignore of checkov errors

--- lambda.tf
+++ lambda.tf
@@ -1,53 +1,24 @@
-resource "aws_iam_role" "health_lambda_iam" {
-  name = var.event_rule_name
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Sid       = ""
-        Principal = { Service = "lambda.amazonaws.com" }
-      },
-    ]
-  })
-  inline_policy {
-    name = "sns"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = ["sns:Publish"]
-          Effect   = "Allow"
-          Resource = var.sns_topic_name != "" ? aws_sns_topic.health_event_topic[0].arn : var.sns_topic_arn
-        },
-        {
-          Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
-          Effect   = "Allow"
-          Resource = "arn:aws:logs:*:*:*"
-        }
-      ]
-    })
-  }
-}
-
 data "archive_file" "health_lambda" {
   type        = "zip"
   source_file = "${path.module}/health-lambda.py"
   output_path = "${path.module}/health-lambda-function-payload.zip"
 }
 
+#checkov:skip=CKV_AWS_117:No need to deploy lambda inside VPC
+#checkov:skip=CKV_AWS_173:No need to encrypt env vars
+#checkov:skip=CKV_AWS_50:Lambda is simple, no need for x-ray
 resource "aws_lambda_function" "health_lambda" {
-  filename         = "${path.module}/health-lambda-function-payload.zip"
-  function_name    = var.event_rule_name
-  role             = aws_iam_role.health_lambda_iam.arn
-  handler          = "health-lambda.lambda_handler"
-  source_code_hash = data.archive_file.health_lambda.output_base64sha256
-  runtime          = "python3.12"
+  filename                       = "${path.module}/health-lambda-function-payload.zip"
+  function_name                  = var.event_rule_name
+  role                           = aws_iam_role.health_lambda_iam.arn
+  reserved_concurrent_executions = 50
+  handler                        = "health-lambda.lambda_handler"
+  source_code_hash               = data.archive_file.health_lambda.output_base64sha256
+  runtime                        = var.runtime
   environment {
     variables = {
-      SNS_TOPIC_ARN   = var.sns_topic_name != "" ? aws_sns_topic.health_event_topic[0].arn : var.sns_topic_arn
-      EVENT_RULE_NAME = var.event_rule_name
+      SNS_TOPIC_ARN        = var.sns_topic_name != "" ? aws_sns_topic.health_event_topic[0].arn : var.sns_topic_arn
+      EVENT_RULE_NAME      = var.event_rule_name
       ALARM_SUBJECT_PREFIX = var.alarm_subject_prefix
     }
   }
@@ -59,10 +30,3 @@ resource "aws_cloudwatch_event_target" "health_lambda" {
   arn       = aws_lambda_function.health_lambda.arn
 }
 
-resource "aws_lambda_permission" "health_lambda" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.health_lambda.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.console.arn
-}
